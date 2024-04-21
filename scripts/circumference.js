55ebb15d150c1b1b486328a11b1dad80547974e4
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-04-21 19:03:25 +0200

    circumference exercise

---
+++ scripts/circumference.js
@@ -0,0 +1,17 @@
+const PI = 3.14159;
+let radius;
+let circumference;
+
+// radius = window.prompt("Enter the radius of circle");
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+
+    circumference = 2 * PI * radius;
+
+    document.getElementById("myH3").textContent = `Circumference = ${circumference}` + " cm";
+
+
+    console.log(`Radius = ${radius}`);
+    console.log(`Circumference = ${circumference}`);
+}
