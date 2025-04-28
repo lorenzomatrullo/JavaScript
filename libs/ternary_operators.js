a37f1c895bc692b0382c7ae04546ee2e783cf666
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-28 11:45:28 +0200

    ternary operators

---
+++ libs/ternary_operators.js
@@ -0,0 +1,16 @@
+//  TERNARY OPERATOR = a shortcut to if-else statements
+//                   helps to assign aa variable based on a condition
+//                   condition ? codeIfTrue : codeIfFalse;
+
+
+let age = 20;
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message);
+
+let time = 9;
+let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+console.log(greeting);
+
+let isDeveloper = true;
+let job = isDeveloper ? "You are a developer" : "You are NOT a developer";
+console.log(job);
